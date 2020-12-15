237bdec80ae8367e2d9be3b4b13bb61d069f7377
Rafael Breno <rafael_breno@outlook.com>
2020-12-15 19:13:40 -0300

    Implement godotenv

--- app/app.go
+++ app/app.go
@@ -1,9 +1,18 @@
 package app
 
 import (
+	"github.com/joho/godotenv"
 	"pulley.com/shakesearch/router"
 )
 
 func Init() {
+	setOS()
+
 	router.Listen()
+}
+
+func setOS() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
 }
