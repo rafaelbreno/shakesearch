9b2d18bddd19cb0daa0f533be4a7e4b4eed690f7
Rafael Breno <rafael_breno@outlook.com>
2020-12-15 19:28:58 -0300

    Add shell file to deploy/execute app

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Checking if is running as sudo/root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Env file
+ENV=.env
+
+# Checking if .env file exists
+if [ ! -f "$ENV" ]; then
+    echo "- $ENV does not exists"
+    echo "- Copying .env.example into .env ..."
+    cp .env.example .env
+    chmod -R 777 .env
+    echo "- Copied!"
+    echo "- Now you can configure the .env file as you wish"
+    exit
+fi
+
+go run main.go
