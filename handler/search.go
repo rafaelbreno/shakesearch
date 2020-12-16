ac13f7b87b611cd9d1446db3673a87f97a702051
Rafael Breno <rafael_breno@outlook.com>
2020-12-16 18:17:26 -0300

    Implement search package

--- handler/search.go
+++ handler/search.go
@@ -4,22 +4,42 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"pulley.com/shakesearch/cmd/search"
 )
 
 type Query struct {
 	Query string `json:"query"`
 }
 
-//type SearchResult struct {
-//}
+var w *search.Works
+var load_error error
+
+func init() {
+	w, load_error = search.Load("completeworks.txt")
+}
 
 func Search(c *gin.Context) {
 	var q Query
+
+	if load_error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": load_error.Error()})
+		return
+	}
 
 	if err := c.BindJSON(&q); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"resp": q})
+	r, err := w.Search(q.Query)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"resp":     r,
+		"contents": w.Contents,
+	})
 }
