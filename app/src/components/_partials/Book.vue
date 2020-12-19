29e0088e607e845e08ecdb60b0936bdf9e6bc3d6
Rafael Breno <rafael_breno@outlook.com>
2020-12-19 19:39:26 -0300

    Add Book component to show Shakespeare's arts

---
+++ app/src/components/_partials/Book.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="position-absolute bottom-0 end-x">
+    <div class="card wid mb-2">
+        <div class="card-body">
+            {{ artTitle }} 
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Book',
+    props: [
+        'artTitle',
+        'artContents'
+    ],
+}
+</script>
+
+<style scoped>
+    .bottom-x {
+        bottom: 0% !important;
+    }
+    .wid {
+        width: 40vw !important;
+    }
+    .end-x {
+        left: 58vw !important;
+    }
+</style>
