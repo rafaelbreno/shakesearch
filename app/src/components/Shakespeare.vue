f0cff93841c2725d5c7febfccf80ea70d3f540ba
Rafael Breno <rafael_breno@outlook.com>
2020-12-19 01:10:30 -0300

    Create component exclusively for Shakespeare portrait

---
+++ app/src/components/Shakespeare.vue
@@ -0,0 +1,21 @@
+<template>
+        <div class="position-absolute bottom-0 end-x">
+            <img src="../assets/svg/shakespeare.svg"
+                 alt="shakespeare"/>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'Shakespeare'
+}
+</script>
+
+<style scoped>
+.bottom-x {
+bottom: 0% !important;
+}
+.end-x {
+right: -3% !important;
+}
+</style>
