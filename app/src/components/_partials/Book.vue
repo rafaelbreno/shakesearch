10216038df16527b82dcf4b6c4a32ea61bd8f64f
Rafael Breno <rafael_breno@outlook.com>
2020-12-19 20:34:07 -0300

    Add iteration to book/art contents

--- app/src/components/_partials/Book.vue
+++ app/src/components/_partials/Book.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="position-absolute bottom-0 end-x">
-    <div class="card wid mb-2">
-        <div class="card-body">
-            {{ artTitle }} 
+    <div class="card wid mb-3 hei">
+        <div class="card-body overflow-y courier">
+            <p class="text-center title">{{ artTitle }}</p>
+            <p v-for="item in artContents" :class="defineClass(item)" :key="item">{{ item }}</p>
         </div>
     </div>
     </div>
@@ -15,6 +16,19 @@ export default {
         'artTitle',
         'artContents'
     ],
+    methods: {
+        defineClass(item) {
+            let spanClass = ''
+            if ( /[0-9]{1}$/.test(item) ) {
+                spanClass = "text-center title"
+            }
+
+            if ( /[A-B]{1,}$/.test(item) ) {
+                spanClass = "title"
+            }
+            return spanClass 
+        }
+    }
 }
 </script>
 
@@ -22,8 +36,23 @@ export default {
     .bottom-x {
         bottom: 0% !important;
     }
+    .hei {
+        height: 96vh !important;
+    }
     .wid {
         width: 40vw !important;
+    }
+
+    .courier {
+        font-family: 'Courier New', monospace;
+    }
+
+    .title {
+        font-weight: 500 !important;
+    }
+
+    .overflow-y {
+        overflow-y: scroll !important;
     }
     .end-x {
         left: 58vw !important;
