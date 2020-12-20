e0c46f899b7788ea201e5c7ee42beba7060e83f8
Rafael Breno <rafael_breno@outlook.com>
2020-12-19 21:49:16 -0300

    Add styling and regex method to bold text

--- app/src/components/_partials/Book.vue
+++ app/src/components/_partials/Book.vue
@@ -3,7 +3,7 @@
     <div class="card wid mb-3 hei">
         <div class="card-body overflow-y courier">
             <p class="text-center title">{{ artTitle }}</p>
-            <p v-for="item in artContents" :class="defineClass(item)" :key="item">{{ item }}</p>
+            <p v-for="item in artContents" :class="defineClass(item)" :key="randomKey(item)">{{ item }}</p>
         </div>
     </div>
     </div>
@@ -19,14 +19,29 @@ export default {
     methods: {
         defineClass(item) {
             let spanClass = ''
-            if ( /[0-9]{1}$/.test(item) ) {
+            // Only numbers
+            if ( /[0-9]{1,}$/.test(item) ) {
                 spanClass = "text-center title"
             }
 
-            if ( /[A-B]{1,}$/.test(item) ) {
-                spanClass = "title"
+            // Only Capitalized letters
+            if ( /[A-Z]{1,}$/.test(item) ) {
+                spanClass = "weight-800"
+            }
+            
+            // Only Capitalized letters with "." at the final
+            if ( /[A-Z]{1,}[.]$/.test(item) ) {
+                spanClass = "weight-600"
+            }
+
+            // Match string where starts withs "[_" and ends with "_]"
+            if ( /[[_]{2}.{1,}[\]_]{2}$/.test(item) ) {
+                spanClass = "weight-600"
             }
             return spanClass 
+        },
+        randomKey(key) {
+            return `${Math.random()}-${key}` 
         }
     }
 }
@@ -51,6 +66,22 @@ export default {
         font-weight: 500 !important;
     }
 
+    .weight-600 {
+        font-weight: 600 !important;
+    }
+    
+    .weight-700 {
+        font-weight: 700 !important;
+    }
+    
+    .weight-800 {
+        font-weight: 800 !important;
+    }
+    
+    .weight-900 {
+        font-weight: 900 !important;
+    }
+    
     .overflow-y {
         overflow-y: scroll !important;
     }
